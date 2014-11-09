ed0a44c12bd0cf2b58777a83e56a703a36905be3
ssikdar1 <shan.sikdar@gmail.com>
2014-11-09 17:10:09 -0500

    Added Skin Detect

--- audio_player.py
+++ audio_player.py
@@ -23,6 +23,24 @@ def adjustAmplitude(data,ampScale):
 
 	return data
 
+def skinDetection(src):
+	""" TODO optimize 
+		http://opencvpython.blogspot.com/2012/06/fast-array-manipulation-in-numpy.html
+	"""
+	cols, rows, dim = original_shape = tuple(src.shape)
+	dst = numpy.zeros(original_shape)
+	for i in range(0,rows):
+		for j in range(0,cols):
+			B = src.item(j,i,0)
+			G = src.item(j,i,1)
+			R = src.item(j,i,2)
+		
+			if(R > 95 and G > 40 and B > 20 and max(R,G,B)-min(R,G,B) > 15 and abs(R-G) > 15 and R > G and R > B):
+				dst.itemset((j,i,0),255)
+				dst.itemset((j,i,1),255)
+				dst.itemset((j,i,2),255)
+	return dst
+
 def main():	
 
 	cap = cv2.VideoCapture(0)
@@ -30,12 +48,19 @@ def main():
 	while(True):
 		# Capture frame-by-frame
 		ret, frame = cap.read()
-
-		# Our operations on the frame come here
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+		skin = skinDetection(frame)
+		contours, hierarchy = cv2.findContours(skin,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+		
+		for h,cnt in enumerate(contours):
+			mask = numpy.zeros(imgray.shape,np.uint8)
+		cv2.drawContours(mask,[cnt],0,255,-1)
+		mean = cv2.mean(im,mask = mask)
+		
+		# # Our operations on the frame come here
+		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		
 		# Display the resulting frame
-		cv2.imshow('frame',gray)
+		cv2.imshow('frame',skin)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
