9f3ba398b4bbe916f991b50b1d05bd5892c1be2d
ssikdar1 <shan.sikdar@gmail.com>
2014-11-09 15:22:38 -0500

    Added Video code commented out audio

--- audio_player.py
+++ audio_player.py
@@ -10,8 +10,11 @@ import sys
 from struct import pack           # This creates the binary data structure for frames
 import numpy
 
-def adjustAmplitude(data):
-	ampScale = .002
+#for video
+import cv2
+
+def adjustAmplitude(data,ampScale):
+	""" get incoming audio data and adjust the amplitude by a certain factor """
 	data_array = numpy.fromstring(data, dtype='h')
 	
 	# Edit
@@ -19,30 +22,49 @@ def adjustAmplitude(data):
 	data = pack('h'*len(data_array), *data_array)
 
 	return data
-		
-CHUNK = 1024
 
-if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
-    sys.exit(-1)
+def main():	
 
-wf = wave.open(sys.argv[1], 'rb')
-data = wf.readframes(CHUNK)
+	cap = cv2.VideoCapture(0)
 
-p = pyaudio.PyAudio()
+	while(True):
+		# Capture frame-by-frame
+		ret, frame = cap.read()
 
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+		# Our operations on the frame come here
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		# Display the resulting frame
+		cv2.imshow('frame',gray)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+# Eventually put the audio stream in the while loop
+# CHUNK = 1024
+
+	# if len(sys.argv) < 2:
+		# print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+		# sys.exit(-1)
+
+	# wf = wave.open(sys.argv[1], 'rb')
+	# data = wf.readframes(CHUNK)
+
+	# p = pyaudio.PyAudio()
+
+	# stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+					# channels=wf.getnchannels(),
+					# rate=wf.getframerate(),
+					# output=True)
 
 
-while data != '':
-	scaledAmp = adjustAmplitude(data)	#scale the volume
-	stream.write(scaledAmp)
-	data = wf.readframes(CHUNK)
+	# while data != '':
+		# scaledAmp = adjustAmplitude(data)	#scale the volume
+		# stream.write(scaledAmp)
+		# data = wf.readframes(CHUNK)
 
-stream.stop_stream()
-stream.close()
+	# stream.stop_stream()
+	# stream.close()
 
-p.terminate()
+	# p.terminate()
+	
+main()
